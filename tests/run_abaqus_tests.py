19519db1ae9eb5ef7f9ee8b94f9bc5176f649c74
Jonathan Stollberg <jonathanstollberg@gmail.com>
2024-12-01 01:58:13 +0100

    new scripts to run tests

---
+++ tests/run_abaqus_tests.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  1 00:04:31 2024
+
+@author: jonat
+"""
+
+"""Simple script to run the Paraqus Abaqus Python tests."""
+
+import paraqus
+import paraqustests as tests
+
+tests.run_abaqus_tests()
---
+++ tests/run_python_tests.py
@@ -0,0 +1,6 @@
+"""Simple script to run the Paraqus Python tests."""
+
+import paraqus
+import paraqustests as tests
+
+tests.run_python_tests()
